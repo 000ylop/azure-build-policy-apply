77cec8da44f4eedef520fac30515c11d8c045f97
poly000 <1348292515@qq.com>
2023-12-07 00:29:59 +0800

    numerous improvements & pipeline name support

---
+++ README.md
@@ -0,0 +1,9 @@
+# azure-build-policy-apply
+
+## dependencies
+
+- azure-cli (+ devops extension)
+- bash
+- jq
+
+Should work on all POSIX-compatible systems (`#!/usr/bin/env`)
--- apply.sh
+++ apply.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
-if [ $# != 4 ]
+if [ $# != 5 ]
 then
     cat <<-EOF
 Usage:
-pat=... $0 {org_url} {proj_name} {repo_name} {branch}
+pat=... $0 {org_url} {proj_name} {repo_name} {branch} {pipeline_name}
 
 Examples:
-    pat=... $0 https://dev.azure.com/azvse aztest aztest bensl/tmpbuild/1201
+    pat=... $0 https://dev.azure.com/azvse aztest aztest bensl/tmpbuild/1201 Overlake-Build-PullRequest
 
 EOF
     exit 1
 fi
+
+export org_url=$1
+export proj_name=$2
+export repo_name=$3
+export branch=$4
+export pipeline_name=$5
 
 if ! [[ ${pat} ]]
 then 
@@ -19,19 +25,26 @@ then
     exit 2
 fi
 
-export org_url=$1
-export proj_name=$2
-export repo_name=$3
-export branch=$4
-
 function get_repo_id {
-  az repos list --org "${org_url}" \
-                --proj aztest | \
+  az repos list \
+    --org "${org_url}" \
+    --query '[].{name:name, id:id}' \
+    --proj "${proj_name}" | \
     jq -r ".[] | select(.name == \"${repo_name}\") | .id"
 }
 # https://unix.stackexchange.com/a/383166
 # export function to subshells for bash
 typeset -fx get_repo_id
+
+function get_pipeline_definition_id {
+  az pipelines build definition list \
+    --org "${org_url}" \
+    --proj "${proj_name}" \
+    --repository "${repo_name}" \
+    --name "${pipeline_name}"  \
+    --query '[0].id'
+}
+typeset -fx get_pipeline_definition_id
 
 function print_policy_list() {
   az repos policy list \
@@ -73,8 +86,7 @@ function setup_build_policy() {
 }
 
 function setup_build_validation() {
-  # TODO: get build definition id
-  build_definition_id=2
+  build_definition_id=$( get_pipeline_definition_id )
   az repos policy build create \
     --blocking true \
     --branch ${branch} \
@@ -86,12 +98,11 @@ function setup_build_validation() {
     --valid-duration 720 \
     --repository-id ${repo_id} \
     --org ${org_url} \
-    --project ${proj_name} \
-
+    --project ${proj_name}
 }
 
 export repo_id=$( get_repo_id )
 
-print_policy_list
+# print_policy_list
 setup_build_policy
 setup_build_validation
